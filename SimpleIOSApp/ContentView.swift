6f62e15e3e3c4bec7e0408e343e12cf32d1ecdf6
Jeong <jeong123wj@gmail.com>
2024-04-30 07:03:05 +0900

    Initial Commit

---
+++ SimpleIOSApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleIOSApp
+//
+//  Created by Woojin on 4/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleIOSApp/SimpleIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleIOSAppApp.swift
+//  SimpleIOSApp
+//
+//  Created by Woojin on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
